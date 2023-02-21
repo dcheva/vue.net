824fc1bd88e40a3b76e56e50a759b4cbf9b062e1
Dmytro Cheva <dmytro.cheva@gmail.com>
2023-02-21 15:25:24 +0200

    update vue2-app from book chapter-12

--- Travel/src/presentation/vue-app/src/router/index.js
+++ Travel/src/presentation/vue-app/src/router/index.js
@@ -1,23 +1,49 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import Home from "@/views/Main/Home";
+import TourLists from "@/views/AdminDashboard/TourLists";
+import TourPackages from "@/views/AdminDashboard/TourPackages";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "Home",
+    component: Home
   },
+  /* lazy loading through dynamic import() */
   {
     path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    name: "About",
+    component: () => import("@/views/Main/About"),
+  },
+  {
+    path: "/admin-dashboard",
+    component: () => import("@/views/AdminDashboard"),
+    children: [
+      {
+        path: "",
+        component: () => import("@/views/AdminDashboard/DefaultContent"),
+      },
+      {
+        path: "weather-forecast",
+        component: () => import("@/views/AdminDashboard/WeatherForecast"),
+      },
+      /* eager loading through static import statement */
+      {
+        path: "tour-lists",
+        component: TourLists,
+      },
+      {
+        path: "tour-packages",
+        component: TourPackages,
+      }
+    ],
+  },
+  {
+    path: "*",
+    redirect: "/",
   },
 ];
 
